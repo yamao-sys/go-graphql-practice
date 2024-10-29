30fd457c646f6b44d1c7e908991e2f88da2f5a15
yamao-sys <yamaopanku@gmail.com>
2024-10-30 02:51:59 +0900

    dataloaderのlint対応

--- app/graph/dataloaders.go
+++ app/graph/dataloaders.go
@@ -1,23 +1,24 @@
 package graph
 
 import (
+	models "app/models/generated"
 	"context"
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/graph-gophers/dataloader/v7"
-	"github.com/kamikazezirou/gql-example/graph/model"
 )
 
 type Loaders struct {
-	UserById *dataloader.Loader[string, *model.User]
+	UserByID *dataloader.Loader[string, *models.User]
 }
 
-// dataloaderはメモリキャッシュを返すらしい オプションでOFFにもできるらしい
+// Middleware dataloaderはメモリキャッシュを返すらしい オプションでOFFにもできるらしい
 func Middleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		ctx := context.WithValue(r.Context(), loadersKey, &Loaders{
-			UserById: dataloader.NewBatchedLoader(func(ctx context.Context, userIds []string) []*dataloader.Result[*model.User] {
+			UserByID: dataloader.NewBatchedLoader(func(ctx context.Context, userIds []string) []*dataloader.Result[*models.User] {
 				fmt.Println("batch get users:", userIds)
 
 				// ユーザIDのリストからユーザ情報を取得する
@@ -25,10 +26,11 @@ func Middleware(next http.Handler) http.Handler {
 				//   - "SELECT * FROM users WHERE id IN (id1, id2, id3, ...)"のようなSQLでDBからユーザ情報を一括取得する
 				//   - 他のサービスのBatch Read APIを呼ぶ
 				// それでN+1問題を回避することができます。
-				results := make([]*dataloader.Result[*model.User], len(userIds))
+				results := make([]*dataloader.Result[*models.User], len(userIds))
 				for i, id := range userIds {
-					results[i] = &dataloader.Result[*model.User]{
-						Data:  &model.User{ID: id, Name: "user " + id},
+					intID, _ := strconv.Atoi(id)
+					results[i] = &dataloader.Result[*models.User]{
+						Data:  &models.User{ID: intID, Name: "user " + id},
 						Error: nil,
 					}
 				}
@@ -45,6 +47,6 @@ type contextKey int
 
 var loadersKey contextKey
 
-func ctxLoaders(ctx context.Context) *Loaders {
+func CtxLoaders(ctx context.Context) *Loaders {
 	return ctx.Value(loadersKey).(*Loaders)
 }

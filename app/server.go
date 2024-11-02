81b042c7726f2228a6e89278c8218abd02f79979
yamao-sys <yamaopanku@gmail.com>
2024-11-02 14:34:15 +0900

    GraphQLのサーバをテストでも使えるようにメソッド化

--- app/server.go
+++ app/server.go
@@ -3,13 +3,12 @@ package main
 import (
 	"app/db"
 	"app/graph"
-	"app/graph/generated"
+	"app/lib"
 	"app/services"
 	"log"
 	"net/http"
 	"os"
 
-	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 )
 
@@ -27,10 +26,9 @@ func main() {
 	// NOTE: service
 	authService := services.NewAuthService(dbCon)
 
-	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: graph.NewResolver(authService)}))
+	srv := lib.GetGraphQLServer(authService)
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	// http.Handle("/query", srv)
 	http.Handle("/query", graph.Middleware(srv))
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
